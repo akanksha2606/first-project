dd8cd77b9e11e0fa7dde712f82df6e74c08d626a
Vedant Datey <65365823+vedant2608@users.noreply.github.com>
2020-07-05 18:47:35 +0530

    This file is related to congo.html

---
+++ congopage.js
@@ -0,0 +1,7 @@
+document.getElementById("clickbtn").onclick=function(){
+     var element=document.getElementById("support");
+    element.classList.toggle("redheart");
+    
+    var done=document.getElementById("afterclick");
+    done.classList.toggle("ho");
+}
